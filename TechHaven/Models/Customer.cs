d9f9f1479b9ed0c88a469d6db3805cb0c6ee56b8
EmirKapic <ekapic1@etf.unsa.ba>
2023-05-11 20:02:56 +0200

    dodan Customer Identity

--- TechHaven/Models/Customer.cs
+++ TechHaven/Models/Customer.cs
@@ -1,33 +1,22 @@
-﻿namespace TechHaven.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechHaven.Models
 {
-    public class Customer : Person, User
+    public class Customer : IdentityUser, Person, User
     {
-        public int CustomerId { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int NumberOfHavenCoins { get; set; }
+        [NotMapped]
         public ShoppingCart ShoppingCart { get; set; }
+        [NotMapped]
         public List<Product> Wishlist { get; set; }
+        [NotMapped]
         public List<Order> Orders { get; set; }
 
-        public Customer(int customerId, string username, string email, string password, string firstName, string lastName, int numberOfHavenCoins, ShoppingCart shoppingCart, List<Product> wishlist, List<Order> orders)
-        {
-            CustomerId = customerId;
-            Username = username;
-            Email = email;
-            Password = password;
-            FirstName = firstName;
-            LastName = lastName;
-            NumberOfHavenCoins = numberOfHavenCoins;
-            ShoppingCart = shoppingCart;
-            Wishlist = wishlist;
-            Orders = orders;
-        }
 
-        public Customer() { }
+        public Customer() { FirstName = ""; LastName = ""; NumberOfHavenCoins = 0; }
     }
 }
 
--- TechHaven/Models/Person.cs
+++ TechHaven/Models/Person.cs
@@ -2,9 +2,6 @@
 {
     public interface Person
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
