a62d2a9ad34efb85552dadda22fda4eaf05b9688
EmirKapic <ekapic1@etf.unsa.ba>
2023-05-15 18:09:05 +0200

    dodane role

--- TechHaven/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TechHaven/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -124,6 +124,7 @@ namespace TechHaven.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "Customer");
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
